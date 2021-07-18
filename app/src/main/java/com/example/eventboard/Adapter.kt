8469fcc8dbe67432f34023590e942edfff83d2c5
Fruttis1337 <Fruttis1337@mail.ru>
2021-07-19 05:39:20 +1000

    filled layout

--- app/src/main/java/com/example/eventboard/Adapter.kt
+++ app/src/main/java/com/example/eventboard/Adapter.kt
@@ -1,6 +1,6 @@
 package com.example.eventboard
 
-import android.media.Image
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
--- app/src/main/java/com/example/eventboard/MainActivity.kt
+++ app/src/main/java/com/example/eventboard/MainActivity.kt
@@ -1,19 +1,57 @@
 package com.example.eventboard
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.eventboard.Interface.RetrofitServices
+import com.example.eventboard.common.Common
 import com.example.eventboard.databinding.ActivityMainBinding
+import dmax.dialog.SpotsDialog
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mService: RetrofitServices
+    lateinit var adapter: Adapter
+    lateinit var dialog: AlertDialog
+    var binding = ActivityMainBinding.inflate(layoutInflater)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding = ActivityMainBinding.inflate(layoutInflater)
+
+        mService = Common.retrofitServices
+        dialog = SpotsDialog.Builder()
+            .setCancelable(true)
+            .setContext(this)
+            .build()
+
+
         val view = binding.root
         setContentView(R.layout.activity_main)
 
+        binding.recycle.setHasFixedSize(true)
         binding.recycle.layoutManager = LinearLayoutManager(this)
-        binding.recycle.adapter = Adapter()
+
+        getAllEvents()
     }
 
+    private fun getAllEvents() {
+        dialog.show()
+        mService.getEvents().enqueue(object : Callback<MutableList<Events>> {
+            override fun onFailure(call: Call<MutableList<Events>>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<MutableList<Events>>, response: Response<MutableList<Events>>) {
+                adapter = Adapter(response.body() as MutableList<Events>)
+                adapter.notifyDataSetChanged()
+                binding.recycle.adapter = adapter
+
+                dialog.dismiss()
+            }
+        })
+    }
 }
